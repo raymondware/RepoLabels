3947407072b49aad67de44d17f6e0b2e083d64aa
raymondware <raymondware.design@gmail.com>
2015-04-21 08:10:41 -0700

    Tue Apr 21 08:10:41 PDT 2015

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import styles from './styles/main.styl'
+import Router from './router'
+import app from 'ampersand-app'
+import Me from './models/me'
+
+app.extend({
+  init () {
+	this.me = new Me()
+    this.router = new Router()
+    this.router.history.start()
+  }
+})
+
+app.init()
+
+window.app = app
---
+++ src/router.js
@@ -0,0 +1,77 @@
+import app from 'ampersand-app'
+import Router from 'ampersand-router'
+import qs from 'qs'
+import React from 'react'
+import PublicPage from './pages/public'
+import ReposPage from './pages/repos'
+import MessagePage from './pages/message'
+import Layout from './layout'
+import xhr from 'xhr'
+
+const auth = function (name) {
+	return function () {
+		if (app.me.isLoggedIn) {
+			this[name].apply(this, arguments)
+		} else {
+			this.redirectTo('/')
+		}
+	}
+}
+
+export default Router.extend({
+  renderPage (Page, opts = {}) {
+    const Main = (
+      <Layout>
+        <Page {...opts}/>
+      </Layout>
+    )
+    React.render(Main, document.body)
+  },
+
+  routes: {
+    '': 'public',
+    'repos': auth('repos'),
+    'login': 'login',
+    'logout': 'logout',
+    'auth/callback?code=:code': 'authCallback',
+    '*404': 'fourOhFour'
+  },
+
+  public () {
+    React.render(<PublicPage/>, document.body)
+  },
+
+  repos () {
+    this.renderPage(ReposPage)
+  },
+
+  login () {
+    window.location = 'https://github.com/login/oauth/authorize?' + qs.stringify({
+      scope: 'user,repo',
+      redirect_uri: window.location.origin + '/auth/callback',
+      client_id: 'f8dd69187841cdd22a26'
+    })
+  },
+
+  authCallback (code) {
+    xhr({
+      url: 'https://labelr-dev.herokuapp.com/authenticate/' + code,
+      json: true
+    }, (err, resp, body) => {
+      if (err) {
+        console.error('bad code')
+      }
+      app.me.token = body.token
+      this.redirectTo('/repos')
+    })
+  },
+  
+  logout () {
+	  window.localStorage.clear()
+	  window.location = '/'
+  },
+  
+  fourOhFour () {
+	  this.renderPage(MessagePage, {title: '404', body: 'Nothing to see here!'})
+  }
+})
